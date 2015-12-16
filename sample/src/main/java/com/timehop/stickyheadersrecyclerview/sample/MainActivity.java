26e0899521481462238ef65bb4ab209cd03701b6
Emanuele Tonello <emanueletonello@gmail.com>
2015-12-17 08:52:00 +1000

    restored indentation to 2 spaces

--- sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/MainActivity.java
+++ sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/MainActivity.java
@@ -24,144 +24,144 @@ import java.security.SecureRandom;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        Button button = (Button) findViewById(R.id.button_update);
-        final ToggleButton isReverseButton = (ToggleButton) findViewById(R.id.button_is_reverse);
+    final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+    Button button = (Button) findViewById(R.id.button_update);
+    final ToggleButton isReverseButton = (ToggleButton) findViewById(R.id.button_is_reverse);
 
-        // Set adapter populated with example dummy data
-        final AnimalsHeadersAdapter adapter = new AnimalsHeadersAdapter();
-        adapter.add("Animals below!");
-        adapter.addAll(getDummyDataSet());
-        recyclerView.setAdapter(adapter);
+    // Set adapter populated with example dummy data
+    final AnimalsHeadersAdapter adapter = new AnimalsHeadersAdapter();
+    adapter.add("Animals below!");
+    adapter.addAll(getDummyDataSet());
+    recyclerView.setAdapter(adapter);
 
-        // Set button to update all views one after another (Test for the "Dance")
-        button.setOnClickListener(new View.OnClickListener() {
+    // Set button to update all views one after another (Test for the "Dance")
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        for (int i = 0; i < adapter.getItemCount(); i++) {
+          final int index = i;
+          handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Handler handler = new Handler(Looper.getMainLooper());
-                for (int i = 0; i < adapter.getItemCount(); i++) {
-                    final int index = i;
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            adapter.notifyItemChanged(index);
-                        }
-                    }, 50);
-                }
+            public void run() {
+              adapter.notifyItemChanged(index);
             }
-        });
-
-        // Set layout manager
-        int orientation = getLayoutManagerOrientation(getResources().getConfiguration().orientation);
-        final LinearLayoutManager layoutManager = new LinearLayoutManager(this, orientation, isReverseButton.isChecked());
-        recyclerView.setLayoutManager(layoutManager);
-
-        // Add the sticky headers decoration
-        final StickyRecyclerHeadersDecoration headersDecor = new StickyRecyclerHeadersDecoration(adapter);
-        recyclerView.addItemDecoration(headersDecor);
-
-        // Add decoration for dividers between list items
-        recyclerView.addItemDecoration(new DividerDecoration(this));
-
-        // Add touch listeners
-        StickyRecyclerHeadersTouchListener touchListener =
-                new StickyRecyclerHeadersTouchListener(recyclerView, headersDecor);
-        touchListener.setOnHeaderClickListener(
-                new StickyRecyclerHeadersTouchListener.OnHeaderClickListener() {
-                    @Override
-                    public void onHeaderClick(View header, int position, long headerId) {
-                        Toast.makeText(MainActivity.this, "Header position: " + position + ", id: " + headerId,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-        recyclerView.addOnItemTouchListener(touchListener);
-        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                adapter.remove(adapter.getItem(position));
-            }
-        }));
-        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-            @Override
-            public void onChanged() {
-                headersDecor.invalidateHeaders();
-            }
-        });
-
-        isReverseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean isChecked = isReverseButton.isChecked();
-                isReverseButton.setChecked(isChecked);
-                layoutManager.setReverseLayout(isChecked);
-                adapter.notifyDataSetChanged();
-            }
-        });
-    }
-
-    private String[] getDummyDataSet() {
-        return getResources().getStringArray(R.array.animals);
-    }
-
-    private int getLayoutManagerOrientation(int activityOrientation) {
-        if (activityOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
-            return LinearLayoutManager.VERTICAL;
-        } else {
-            return LinearLayoutManager.HORIZONTAL;
+          }, 50);
         }
-    }
+      }
+    });
 
-    private class AnimalsHeadersAdapter extends AnimalsAdapter<RecyclerView.ViewHolder>
-            implements StickyRecyclerHeadersAdapter<RecyclerView.ViewHolder> {
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.view_item, parent, false);
-            return new RecyclerView.ViewHolder(view) {
-            };
-        }
+    // Set layout manager
+    int orientation = getLayoutManagerOrientation(getResources().getConfiguration().orientation);
+    final LinearLayoutManager layoutManager = new LinearLayoutManager(this, orientation, isReverseButton.isChecked());
+    recyclerView.setLayoutManager(layoutManager);
 
-        @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            TextView textView = (TextView) holder.itemView;
-            textView.setText(getItem(position));
-        }
+    // Add the sticky headers decoration
+    final StickyRecyclerHeadersDecoration headersDecor = new StickyRecyclerHeadersDecoration(adapter);
+    recyclerView.addItemDecoration(headersDecor);
 
-        @Override
-        public long getHeaderId(int position) {
-            if (position == 0) {
-                return -1;
-            } else {
-                return getItem(position).charAt(0);
-            }
-        }
+    // Add decoration for dividers between list items
+    recyclerView.addItemDecoration(new DividerDecoration(this));
 
-        @Override
-        public RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.view_header, parent, false);
-            return new RecyclerView.ViewHolder(view) {
-            };
-        }
-
-        @Override
-        public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
-            TextView textView = (TextView) holder.itemView;
-            textView.setText(String.valueOf(getItem(position).charAt(0)));
-            holder.itemView.setBackgroundColor(getRandomColor());
-        }
-
-        private int getRandomColor() {
-            SecureRandom rgen = new SecureRandom();
-            return Color.HSVToColor(150, new float[]{
-                    rgen.nextInt(359), 1, 1
+    // Add touch listeners
+    StickyRecyclerHeadersTouchListener touchListener =
+            new StickyRecyclerHeadersTouchListener(recyclerView, headersDecor);
+    touchListener.setOnHeaderClickListener(
+            new StickyRecyclerHeadersTouchListener.OnHeaderClickListener() {
+              @Override
+              public void onHeaderClick(View header, int position, long headerId) {
+                Toast.makeText(MainActivity.this, "Header position: " + position + ", id: " + headerId,
+                        Toast.LENGTH_SHORT).show();
+              }
             });
-        }
+    recyclerView.addOnItemTouchListener(touchListener);
+    recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
+      @Override
+      public void onItemClick(View view, int position) {
+        adapter.remove(adapter.getItem(position));
+      }
+    }));
+    adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+      @Override
+      public void onChanged() {
+        headersDecor.invalidateHeaders();
+      }
+    });
 
+    isReverseButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        boolean isChecked = isReverseButton.isChecked();
+        isReverseButton.setChecked(isChecked);
+        layoutManager.setReverseLayout(isChecked);
+        adapter.notifyDataSetChanged();
+      }
+    });
+  }
+
+  private String[] getDummyDataSet() {
+    return getResources().getStringArray(R.array.animals);
+  }
+
+  private int getLayoutManagerOrientation(int activityOrientation) {
+    if (activityOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+      return LinearLayoutManager.VERTICAL;
+    } else {
+      return LinearLayoutManager.HORIZONTAL;
     }
+  }
+
+  private class AnimalsHeadersAdapter extends AnimalsAdapter<RecyclerView.ViewHolder>
+          implements StickyRecyclerHeadersAdapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+      View view = LayoutInflater.from(parent.getContext())
+              .inflate(R.layout.view_item, parent, false);
+      return new RecyclerView.ViewHolder(view) {
+      };
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+      TextView textView = (TextView) holder.itemView;
+      textView.setText(getItem(position));
+    }
+
+    @Override
+    public long getHeaderId(int position) {
+      if (position == 0) {
+        return -1;
+      } else {
+        return getItem(position).charAt(0);
+      }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+      View view = LayoutInflater.from(parent.getContext())
+              .inflate(R.layout.view_header, parent, false);
+      return new RecyclerView.ViewHolder(view) {
+      };
+    }
+
+    @Override
+    public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
+      TextView textView = (TextView) holder.itemView;
+      textView.setText(String.valueOf(getItem(position).charAt(0)));
+      holder.itemView.setBackgroundColor(getRandomColor());
+    }
+
+    private int getRandomColor() {
+      SecureRandom rgen = new SecureRandom();
+      return Color.HSVToColor(150, new float[]{
+              rgen.nextInt(359), 1, 1
+      });
+    }
+
+  }
 }
