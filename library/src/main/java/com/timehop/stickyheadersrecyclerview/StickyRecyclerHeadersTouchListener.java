6960951053b9d025822db5b7ae50257be63e2886
jianjun.lin <jianjun.lin@baidao.com>
2016-12-29 14:51:01 +0800

    support click child view in sticky header

--- library/src/main/java/com/timehop/stickyheadersrecyclerview/StickyRecyclerHeadersTouchListener.java
+++ library/src/main/java/com/timehop/stickyheadersrecyclerview/StickyRecyclerHeadersTouchListener.java
@@ -1,10 +1,13 @@
 package com.timehop.stickyheadersrecyclerview;
 
+import android.graphics.Rect;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.SoundEffectConstants;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class StickyRecyclerHeadersTouchListener implements RecyclerView.OnItemTouchListener {
   private final GestureDetector mTapDetector;
@@ -68,12 +71,43 @@ public class StickyRecyclerHeadersTouchListener implements RecyclerView.OnItemTo
       if (position != -1) {
         View headerView = mDecor.getHeaderView(mRecyclerView, position);
         long headerId = getAdapter().getHeaderId(position);
+        Rect rect = mDecor.getHeaderRect(position);
+        int hOffset = rect.left - headerView.getLeft();
+        int vOffset = rect.top - headerView.getTop();
+        if (isChildClicked(headerView, hOffset, vOffset, e)) {
+          return false;
+        }
         mOnHeaderClickListener.onHeaderClick(headerView, position, headerId);
         mRecyclerView.playSoundEffect(SoundEffectConstants.CLICK);
         headerView.onTouchEvent(e);
         return true;
       }
       return false;
+    }
+
+    private boolean isChildClicked(View headerView, int hOffset, int vOffset, MotionEvent e) {
+      if (headerView instanceof ViewGroup == false) {
+        return false;
+      }
+      ViewGroup vg = (ViewGroup) headerView;
+      int childCount = vg.getChildCount();
+      for (int i = 0; i < childCount; i++) {
+        View child = vg.getChildAt(i);
+        if (isViewTouched(child, hOffset, vOffset, e) && child.isClickable()) {
+          child.performClick();
+          return true;
+        }
+      }
+      return false;
+    }
+
+    private boolean isViewTouched(View view, int hOffset, int vOffset, MotionEvent e) {
+      Rect rect = new Rect();
+      rect.left = view.getLeft() + hOffset;
+      rect.right = view.getRight() + hOffset;
+      rect.top = view.getTop() + vOffset;
+      rect.bottom = view.getBottom() + vOffset;
+      return rect.contains((int) e.getX(), (int) e.getY());
     }
 
     @Override
